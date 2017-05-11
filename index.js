acc90c3e66e4ae244dd3fe099934ecdf7857bc02
Nick Ronnei <nronnei@gmail.com>
2017-05-11 18:53:53 -0400

    Adds the html and styles the page.

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
   next();
 });
 
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(8080);
-console.log('meet-irl is running on 8080');
+console.log('angular-challenge is running on 8080');
