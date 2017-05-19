4a62e233bfba9588ae8f18e0dbab959385768dae
kpfein <kpfein@yahoo.com>
2017-05-19 01:36:21 -0500

    cleanup and proofread

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-angular.module('fibonacciFunTimeApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider, $httpProvider){
+angular.module('fibonacciFunTimeApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider){
 
   $urlRouterProvider.otherwise('/simple');
 
   $stateProvider
+  	.state('simple', {
+  		url: '/simple',
+      controller: 'simpleFibController',
+  		templateUrl: 'simple/simpleFib.html'
+  	})
+  	.state('iterative', {
+  		url: '/iterative',
+      controller: 'iterativeFibController',
+  		templateUrl: 'iterative/iterativeFib.html'
+  	})
+  	.state('recursive', {
+  		url: '/recursive',
+      controller: 'recursiveFibController',
+  		templateUrl: 'recursive/recursiveFib.html'
+  	})
 
-	.state('simple', {
-		url: '/simple',
-    controller: 'simpleFibController',
-		templateUrl: 'simple/simpleFib.html'
-	})
-	.state('iterative', {
-		url: '/iterative',
-    controller: 'iterativeFibController',
-		templateUrl: 'iterative/iterativeFib.html'
-	})
-	.state('recursive', {
-		url: '/recursive',
-    controller: 'recursiveFibController',
-		templateUrl: 'recursive/recursiveFib.html'
-	})
-
-});
+}]);
